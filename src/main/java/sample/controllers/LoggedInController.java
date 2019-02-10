2ca8e95868ce4b5e951d0eab36b1a40bff283ad4
Sentrilio <dominik.kowalski@protonmail.com>
2019-02-10 01:50:55 +0100

    something is wrong screen is not loading

--- src/main/java/sample/controllers/LoggedInController.java
+++ src/main/java/sample/controllers/LoggedInController.java
@@ -2,6 +2,7 @@ package sample.controllers;
 
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -36,19 +37,19 @@ public class LoggedInController implements Initializable, ControlledScreen {
 	@FXML
 	private ComboBox<String> presenceComboBox;
 
-//	@FXML
-//	private Button refreshButton;
-
 	@FXML
 	private Button logoutButton;
 
 	@FXML
 	private Button sendButton;
+
 	@FXML
 	private VBox VBox;
 
-	@FXML
-	private TextField destName;
+//	@FXML
+//	private TextField destName;
+
+	private String dest=xmppSession.userAccount.getLogin();//default to himself
 
 	@FXML
 	public TextArea conversationField;
@@ -62,54 +63,34 @@ public class LoggedInController implements Initializable, ControlledScreen {
 	@FXML
 	private VBox VBoxFriendList;
 
-
 	private List<Region> controls = new ArrayList<>();
+
+	private HashMap<String, TextField> mapOfConversations = new HashMap<>();
+	private HashMap<String, String> mapOfFriends = new HashMap<>();
 
 	@FXML
 	public void initialize(URL location, ResourceBundle resources) {
-//		controls.add(refreshButton);
 		controls.add(logoutButton);
 		controls.add(sendButton);
 		controls.add(sendButton);
-		controls.add(VBox);
-		controls.add(destName);
+//		controls.add(VBox);
+//		controls.add(destName);
 		controls.add(conversationField);
 		controls.add(sendTextField);
-//		controls.add(tableColumn);
-//		for (Region r : controls) {
-//			if (r != refreshButton) {
-//				r.setVisible(false);
-//			}
-//		}
+//		for (Region r : controls) {}
+
 		anchorPane.setOnKeyPressed(event -> {
 			if (event.getCode() == KeyCode.ENTER) {
 				System.out.println("kliknięto enter");
 				try {
-//					if (!refreshButton.isVisible()) {
-						System.out.println("Proba wysłania wiadomości");
-						sendMessage();
-//					}
+					System.out.println("Proba wysłania wiadomości");
+					sendMessage();
 				} catch (XmppStringprepException | SmackException.NotConnectedException | InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-
 		});
-//		refreshButton.setOnKeyReleased(event -> {
-//			if (event.getCode() == KeyCode.ENTER) {
-//				if (refreshButton.isVisible()) {
-//					System.out.println("connected to the server");
-//					try {
-//						setController();
-//					} catch (IOException | SmackException.NotConnectedException | InterruptedException e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			}
-//		});
-
 	}
-
 
 	@FXML
 	void logoutButtonClick(ActionEvent event) {
@@ -126,7 +107,8 @@ public class LoggedInController implements Initializable, ControlledScreen {
 		String message = sendTextField.getText();
 		sendTextField.clear();
 		conversationField.appendText("me: " + message + "\n");
-		String jidString = destName.getText();
+//		String jidString = destName.getText();
+		String jidString = getNameFromFriendList(dest);
 		jidString += "@" + xmppSession.xmppDomain;
 		EntityBareJid jid = JidCreate.entityBareFrom(jidString);
 		Chat chat = xmppSession.chatManager.chatWith(jid);
@@ -148,11 +130,17 @@ public class LoggedInController implements Initializable, ControlledScreen {
 	public void setScreenParent(ScreensController screenParent) {
 		screensController = screenParent;
 	}
-
-//	@FXML
-//	void refreshButtonClick(ActionEvent event) throws IOException, SmackException.NotConnectedException, InterruptedException {
-//		setController();
-//	}
+	public String getNameFromFriendList(String nameWithStatus){
+		String name = "";
+		for (char c : nameWithStatus.toCharArray()) {
+			if (c != '(') {
+				name += c;
+			} else {
+				break;
+			}
+		}
+		return name;
+	}
 
 	public void setController() throws IOException, SmackException.NotConnectedException, InterruptedException {
 
@@ -199,7 +187,7 @@ public class LoggedInController implements Initializable, ControlledScreen {
 //			if (r == refreshButton) {
 //				r.setVisible(false);
 //			} else {
-				r.setVisible(true);
+			r.setVisible(true);
 //			}
 		}
 		Presence p = new Presence(Presence.Type.available, "I'm not busy", 42, Presence.Mode.dnd);
@@ -230,18 +218,28 @@ public class LoggedInController implements Initializable, ControlledScreen {
 		Collection<RosterEntry> entries = xmppSession.roster.getEntries();
 		for (RosterEntry entry : entries) {
 			Button button = new Button();
+//			button.setOnAction(new EventHandler<ActionEvent>() {
+//				public void handle(ActionEvent e) {
+//					dest=getNameFromFriendList(button.getText());
+//					System.out.println("clicked" + dest);
+//				}
+//			});
 			Presence s = xmppSession.roster.getPresence(entry.getJid());
-			button.setText(getNameFromJid((EntityBareJid) entry.getJid()) + ": " + s.getStatus());
+			button.setText(getNameFromJid((EntityBareJid) entry.getJid()) + "(" + s.getStatus()+")");
 			VBoxFriendList.getChildren().add(button);
 			System.out.println("entry: " + entry);
 		}
 
-		for(int i=0;i<20;i++){
-			Button button = new Button();
-			button.setText("test " +i);
-
-			VBoxFriendList.getChildren().add(button);
-		}
+//		for (int i = 0; i < 20; i++) {
+//			Button button = new Button();
+//			button.setOnAction(new EventHandler<ActionEvent>() {
+//				public void handle(ActionEvent e) {
+//					System.out.println("clicked" + button.getText());
+//				}
+//			});
+//			button.setText("test " + i);
+//			VBoxFriendList.getChildren().add(button);
+//		}
 		presenceComboBox.getItems().addAll(
 				"available",
 				"chat",
@@ -276,11 +274,5 @@ public class LoggedInController implements Initializable, ControlledScreen {
 	}
 
 
-//	@FXML
-//	void presenceChanged(ActionEvent event) {
-//		Presence p = new Presence(presenceMenu.ge, "I'm not busy", 42, Presence.Mode.dnd);
-//		xmppSession.connection.sendStanza(p);
-//		presenceMenu.setText();
-//		presenceMenu.setText(presenceMenu.getUserData().toString());
-//	}
+
 }
